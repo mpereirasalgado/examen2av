fdaf3f80bc248898c4fb490d0e5edf3121f9be34
micaelr <micaelr@192.168.56.1>
2015-03-19 09:21:23 +0100

    segundo intento

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -31,5 +31,6 @@ public class StatusTwitter {
             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
         }
         //ya tengo la rama creada
+        //segundo intento commit
     }
 }
